f5c8e44266e16f7874c0eeeb0c6c1b1cadb80add
Lucia <jung991221@naver.com>
2024-03-22 09:54:23 +0900

    text editor 생성

--- view_test/View/new/writeAllView.swift
+++ view_test/View/new/writeAllView.swift
@@ -11,45 +11,54 @@ struct writeAllView: View {
     @Binding var showAllWrite: Bool
     @State private var openPhoto = false
     @State private var image = UIImage()
+    @State var text: String = ""
     
     var body: some View {
         ZStack{
             
-            Color(#colorLiteral(red: 0.8106445074081421, green: 0.9772773385047913, blue: 1, alpha: 1)).ignoresSafeArea()
-            VStack{
-                HStack{
-                    Button(action: {
-                        self.showAllWrite = false
-                    }) {
-                        Text("back")
-                            .frame(width: 300, alignment: .leading)
+            ScrollView {
+                
+                Color(#colorLiteral(red: 0.8106445074081421, green: 0.9772773385047913, blue: 1, alpha: 1)).ignoresSafeArea()
+                VStack{
+                    HStack{
+                        Button(action: {
+                            self.showAllWrite = false
+                        }) {
+                            Text("back")
+                                .frame(width: 300, alignment: .leading)
+                        }
+                        
                     }
                     
-                }
-                
-                //사진
-                NavigationView {
-                    VStack {
-                        Image(uiImage: self.image)
-                            .resizable()
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                    }
-                    .navigationBarTitle("사진", displayMode: .inline)
+                    //사진
+                    NavigationView {
+                        VStack {
+                            Image(uiImage: self.image)
+                                .resizable()
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                        }
+                        .navigationBarTitle("사진", displayMode: .inline)
                         .navigationBarItems(trailing:
-                                Button(action: {
-                                    self.openPhoto = true
-                                }) {
-                                    Text("사진 추가")
-                                    }
+                                                Button(action: {
+                            self.openPhoto = true
+                        }) {
+                            Text("사진 추가")
+                        }
                         )
-                }
-                .sheet(isPresented: $openPhoto) {
-                    ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
-                }
-                
-                
-                
-                Text("기록")
+                    }
+                    .sheet(isPresented: $openPhoto) {
+                        ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+                    }
+                    
+                    
+                    
+                    Text("기록")
+                    TextEditor(text: $text)
+                        .border(.black)
+                        .padding(20)
+                        .frame(height: 200)
+                    
+                    }
             }
         }
     }
